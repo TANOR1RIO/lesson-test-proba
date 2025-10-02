a1306afc97d6a5f39c6ecdea7cbc4103e06e271b
Evgeniy <evgeniyvaxruhsev@gmail.com>
2025-10-02 18:30:07 +0500

    add project files

---
+++ app/ui/fonts.ts
@@ -0,0 +1,8 @@
+// Используем системные шрифты вместо Google Fonts для избежания проблем с сетью
+export const inter = {
+  className: 'font-sans'
+};
+
+export const lusitana = {
+  className: 'font-serif'
+};
